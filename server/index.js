efa5825f8d27b7f89d7476cf948141a3f05573fa
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-19 18:29:54 +0545

    sign up functionality

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cors from "cors";
 dotenv.config();
 const app = express();
 
@@ -19,6 +20,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`I am at server PORT : ${PORT}`);
