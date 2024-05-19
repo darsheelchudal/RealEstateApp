f6f7b133f40b184e1b429ef1591e899043005ee3
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-19 12:07:05 +0545

    React Router DOM For RealEstate page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div>Abut</div>
+    </>
+  );
+}
+
+export default About;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <>
+      <div>Sign up</div>
+    </>
+  );
+}
+export default SignUp;
