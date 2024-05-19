6c04f95e7fe6fa0d475a7617bd1ae8f6e9259fd4
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-19 14:20:56 +0545

    Connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { log } from "console";
+dotenv.config();
 const app = express();
 
-const PORT = 8000;
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MONGODB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(PORT, () => {
   console.log(`I am at server PORT : ${PORT}`);
