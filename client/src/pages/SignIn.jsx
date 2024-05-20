e03afae6500ab661865c6268c5230518f96fbeeb
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-20 20:56:26 +0545

    Using redux to store users

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,23 +1,33 @@
+// SignIn.jsx
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInFailure,
+  signInSuccess,
+} from "../redux/user/userSlice";
 
 function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error, currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("http://localhost:8000/api/auth/sign-in", {
         method: "POST",
         headers: {
@@ -26,19 +36,17 @@ function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
-      if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+      console.log("API Response Data:", data); // Log the received data
 
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+
+      dispatch(signInSuccess(data)); // Assuming data contains the user object
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
