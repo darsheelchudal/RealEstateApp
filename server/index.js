53e1684cdcb7f77881f3d5d3fb11e534447d4367
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-23 07:13:12 +0545

    userUpdate usng Api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 dotenv.config();
 const app = express();
@@ -20,6 +21,8 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
